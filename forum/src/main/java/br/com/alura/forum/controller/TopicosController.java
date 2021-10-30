eb39557ef053eff2f5ca04037ac79c78ce594ae1
lacroixbr <52974728+lacroixbr@users.noreply.github.com>
2021-10-29 22:53:49 -0300

    Update TopicosController.java
    Alterado o método listaTopicos() para trazer uma lista do banco de dados e com parametros

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,31 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
-	public List<TopicoDTO> listaTopicos(){
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("programação", "Spring"));
+	public List<TopicoDTO> listaTopicos(String nomeCurso){
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDTO.converter(topicos);			
+		}else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDTO.converter(topicos);
+		}
 		
-		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
 	}
-
+	
 }
